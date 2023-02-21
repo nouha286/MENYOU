e752d702de370309cf3c3e40279e043b278ec56c
nouha286 <nouhailaelaalami286@gmail.com>
2023-02-21 15:03:48 +0100

    Add containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.orange[100],
         appBar: AppBar(
           title: Text("MenYOU"),
           backgroundColor: Colors.orange[900],
         ),
-        body: Center(
-            child: Image(
-          image: AssetImage("images/logo.png"),
-        )),
+        body: SafeArea(
+          child: Container(
+              child: Image(
+            image: AssetImage("images/logo.png")
+          )),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
