fbc50d12f934094fb0d24a91e4ca2f4fa57d180c
nouha286 <nouhailaelaalami286@gmail.com>
2023-02-21 11:40:16 +0100

    Add pectures

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.orange[100],
         appBar: AppBar(
-          title:Text("MenYOU"),
+          title: Text("MenYOU"),
           backgroundColor: Colors.orange[900],
         ),
+        body: Center(
+            child: Image(
+          image: AssetImage("images/logo.png"),
+        )),
       ),
     ),
   );
